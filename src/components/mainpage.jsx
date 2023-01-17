a513daf4e56495d7279b30fc0d02aac832bc348f
Fadi-Aldalati <fadi.aldalati.2510@gmail.com>
2023-01-17 09:13:59 +0300

    replacing with hooks completed

--- src/components/mainpage.jsx
+++ src/components/mainpage.jsx
@@ -1,9 +1,10 @@
-import React, { Component} from 'react';
-import { saveTask ,getTasks,deleteTask} from '../services/taskService';
+import React, { useState, useEffect } from "react";
+import { saveTask, getTasks, deleteTask } from "../services/taskService";
 
-
-class Main extends Component {
-    constructor(props) {
+const Main = () => {
+  const [title, setTitle] = useState("");
+  const [tasks, setTasks] = useState();
+  /*   constructor(props) {
         super(props);
     
         this.state = {
@@ -14,79 +15,94 @@ class Main extends Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
     }
-    
-    async componentDidMount(){
-        const data = await (await getTasks()).data;
-        const tasks=[...data];
-        this.setState({tasks:tasks});
-    };
-    async handleDelete(task){
-        const tasks = this.state.tasks.filter(m => m._id !== task._id);
-        this.setState({ tasks });
-        try{
-            await deleteTask(task._id);
-             }
-             catch(err){
-                console.log('deleting task error',err);
-             }  
-            
+    */
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await (await getTasks()).data;
+      console.log(data);
+      const tasks = [...data];
+      setTasks(tasks);
     }
-    handleChange(e){
-        this.setState({
-            title: e.target.value
-        });
-        
+    fetchData();
+  }, []);
+
+  async function handleDelete(task) {
+    const newTasks = tasks.filter((m) => m._id !== task._id);
+    setTasks(newTasks);
+    try {
+      await deleteTask(task._id);
+    } catch (err) {
+      console.log("deleting task error", err);
     }
-    async handleSubmit(event) {
-        event.preventDefault();
-        
-        try{
-            if(this.state.title!==''){
+  }
+
+  function handleChange(e) {
+    setTitle(e.target.value);
+  }
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      if (title !== "") {
+        console.log(title);
         const demo = {
-            title:this.state.title,
-        }
+          title: title,
+        };
+
         const task = await (await saveTask(demo)).data;
-        const tasks=[...this.state.tasks,task];
-        this.setState({tasks});
+
+        const newTasks = [...tasks, task];
+        console.log(newTasks);
+        setTasks(newTasks);
+      }
+    } catch (err) {
+      console.log("submiting task error", err);
     }
-        
-         }
-         catch(err){
-            console.log('submiting task error',err);
-         }    
-         event.target.reset();
-         this.setState({title:''});
-    }
-    
-    render() { 
-        const{tasks}=this.state;
-        return (
-            <React.Fragment>
-                <div className='container' style={{"marginTop":"20px"}}>
-                    <h2>Add Item</h2>
-                    <form onSubmit={this.handleSubmit}>
-                    <div className="input-group">
-                        <div  className="form-outline">
-                        <input type="text" onChange={this.handleChange} placeholder="Type here..." className="form-control"/>
-                        </div>
-                        <button className="btn btn-primary">Submit</button>
-                    </div>
-                    </form>
-                    <br/>
-                    <ul className="list-group">
-                    {tasks.map(task =>
-                        <li key={task._id} className="list-group-item d-flex justify-content-between align-items-center">
-                            {task.title}
-                            <div style={{"textAlign":"right"}}>
-                            <button type="text" onClick={e =>this.handleDelete(task)} className="btn btn-danger">Delete</button>
-                            </div>
-                        </li>
-                        )}
-                    </ul>
-                </div>
-            </React.Fragment>
-        );
-    }
-}
- 
+    event.target.reset();
+    setTitle("");
+  }
+
+  return (
+    <React.Fragment>
+      <div className="container" style={{ marginTop: "20px" }}>
+        <h2>Add Item</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="form-outline">
+              <input
+                type="text"
+                onChange={handleChange}
+                placeholder="Type here..."
+                className="form-control"
+              />
+            </div>
+            <button className="btn btn-primary">Submit</button>
+          </div>
+        </form>
+        <br />
+        <ul className="list-group">
+          {tasks?.map((task) => (
+            <li
+              key={task._id}
+              className="list-group-item d-flex justify-content-between align-items-center"
+            >
+              {task.title}
+              <div style={{ textAlign: "right" }}>
+                <button
+                  type="text"
+                  onClick={()=>handleDelete(task)}
+                  className="btn btn-danger"
+                >
+                  Delete
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </React.Fragment>
+  );
+};
+
 export default Main;
